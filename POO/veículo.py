e433726522a2ce5b92a3b45125f0339b67d77a51
Lucas Vassalo <lv.cristiano@hotmail.com>
2024-08-30 17:19:54 -0300

    continuação dos estudos de programação orientada a objetos

---
+++ POO/veículo.py
@@ -0,0 +1,8 @@
+class Veiculo:
+    def __init__(self, marca, modelo, ano, quilometragem):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.quilometragem = quilometragem
+    
+    def dirigir():
