988fdd377cdb4b33aa86c8e8e961ffe06a3977d3
Lucas Vassalo <138052789+VassaloSama@users.noreply.github.com>
2024-09-16 17:12:09 -0300

    Finalização da atividade

--- POO/pré atividade/ex2.py
+++ POO/pré atividade/ex2.py
@@ -5,7 +5,7 @@ class Pessoa:
     
     def verificar_idade(self, value):
         while value < 18:
-            print('Idade inferior a 18 anos')
+            print(f' Funcionario {self.nome} tem idade inferior a 18 anos')
             value = int(input('Informe a idade:'))
         self.idade = value
 
@@ -25,5 +25,40 @@ class Funcionario(Pessoa):
     
     def calcular_salario_anual(self):
         return self.__salario * 12
+
+class Departamento:
+    def __init__ (self, nome):
+        self.nome = nome
+        self.funcionarios = []
+    
+    def adicionar_funcionario(self, funcionario):
+        self.funcionarios.append(funcionario)
+    
+    def calcular_total_salarios(self):
+        if len(self.funcionarios) > 0:
+            total = 0
+            for funcionario in self.funcionarios:
+                total += funcionario.calcular_salario_anual()
+            return total
+        else:
+            print('Departamento sem funcionários')
+    
+    def listar_funcionarios(self):
+        if len(self.funcionarios) > 0:
+            print(f'Funcionários:')
+            for funcionario in self.funcionarios:
+                print(f'Nome: {funcionario.nome}')
+        else:
+            print('Não possui nenhum funcionario')
      
-Lucas = Funcionario ('Lucas',5,200)
+
+Marcio= Funcionario('Marcio', 30, 6000)
+Alex= Funcionario('Alex', 40, 10000)
+
+TI = Departamento('Tecnologia da Informação')
+
+TI.adicionar_funcionario(Marcio)
+TI.adicionar_funcionario(Alex)
+TI.listar_funcionarios()
+TI.calcular_total_salarios(TI)
+
